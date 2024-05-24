42bd558f91c0ef4fb8ceb4c9e681cd9dd3829750
Merabi Samkharadze <samkharadzemerab@gmail.com>
2024-05-24 18:10:00 +0400

    returning a value

--- script.js
+++ script.js
@@ -13,3 +13,29 @@ let result2 = showCount();
 console.log(result);
 console.log(result1);
 console.log(result2);
+
+// Returning a value
+
+function sum(x, y) {
+  return x + y;
+}
+
+let sumResult = sum(190, 20);
+console.log(sumResult);
+
+////////////////////////////
+
+function checkAge(age) {
+  if (age >= 18) {
+    return true;
+  } else {
+    return confirm("Do you have permision from your parents?");
+  }
+}
+let age = prompt("Your age: ");
+
+if (checkAge(age)) {
+  alert("Access granted!");
+} else {
+  alert("Access denied!");
+}
