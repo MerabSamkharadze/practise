388c6f633aea243f64568925264a4a3c47b136d8
Merabi Samkharadze <samkharadzemerab@gmail.com>
2024-05-25 00:25:29 +0400

    arrow function

--- script.js
+++ script.js
@@ -143,3 +143,27 @@ getGender(
     alert("you are woman");
   }
 );
+
+//arrow function
+
+function ask(question, yes, no) {
+  if (confirm(question)) yes();
+  else no();
+}
+
+ask(
+  "Do you agree?",
+  function () {
+    alert("You agreed.");
+  },
+  function () {
+    alert("You canceled the execution.");
+  }
+);
+
+let ask = (questen, yes, no) => (confirm(questen) ? yes() : no());
+ask(
+  "Do you agree?",
+  () => alert("You agreed."),
+  () => alert("You canceled the execution.")
+);
